5df436bdb508dd68a2f6bed1a156170e5600cef6
syedraahim <syedraahim98@gmail.com>
2019-03-10 14:04:44 +0500

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
-var config = {
-    apiKey: "AIzaSyCSynStyKGX_mwurasiANKNnb5ckYB02zA",
-    authDomain: "quiz-application-3bad7.firebaseapp.com",
-    databaseURL: "https://quiz-application-3bad7.firebaseio.com",
-    projectId: "quiz-application-3bad7",
-    storageBucket: "quiz-application-3bad7.appspot.com",
-    messagingSenderId: "296984371492"
-};
+firebase code
+//
+//
 var fire = firebase.initializeApp(config);
 export default fire;
